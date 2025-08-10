bd45d4e05e5caeb4dea6e33bf2cad0d1a20adbb5
yougotborked <anbork@gmail.com>
2025-08-09 20:24:51 -0500

    Override vibration plugin to modern version

---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,54 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get serverUrlLabel => 'Server URL';
+
+  @override
+  String get usernameLabel => 'Username';
+
+  @override
+  String get passwordLabel => 'Password';
+
+  @override
+  String hello(String username) {
+    return 'Hello $username';
+  }
+
+  @override
+  String get library => 'Library';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get continueListening => 'Continue Listening';
+
+  @override
+  String get refresh => 'Refresh';
+
+  @override
+  String get errorLoadingData => 'Error loading data';
+
+  @override
+  String get controls => 'Controls';
+
+  @override
+  String get localListening => 'Local Listening';
+
+  @override
+  String get downloads => 'Downloads';
+
+  @override
+  String get noDownloads => 'No downloads';
+}
